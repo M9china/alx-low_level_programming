a2a1b08f9cf7ede159d47dadc9d53a2086ff8a97
M9china <nqobilencaza2@gmail.com>
2023-02-17 02:43:57 -0800

    C thats puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,16 @@
 
 #include<stdio.h>
+?
 /**
 *
 *main - entry point
 *
-return - always 0
+* return - always 0
 */
-int main(void){
-puts("Programming is like building a multilingual puzzle"\");
 
-return (0);
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+
+retun (0);
 }
