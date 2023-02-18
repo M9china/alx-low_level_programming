706d7c91723f00497827af2edb932d6e8c09ef59
M9china <nqobilencaza2@gmail.com>
2023-02-18 05:00:37 -0800

    C that puts

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,16 @@
 
-#include<stdio.h>
-
+/**
+* file: printf.c
+*/
+#include <stdio.h>
+/**
+* main - prints with proper grammar, but the outcome is a piece of art,
+*
+* return: Always 0
+*/
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,
+    printf("with proper grammar, but the output is a piece of art,
 ");
-
 return (0);
 }
