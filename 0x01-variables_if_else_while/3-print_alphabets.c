0f9d45f86ff9489648ebc1f919076583aa551b24
M9china <95371889+M9china@users.noreply.github.com>
2023-02-21 03:13:41 +0200

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
 #include <ctype.h>
 /**
- *main - Entry point
- *Retrun: Always 0 (succes)
- */
+*main - Entry point
+*Retrun: Always 0 (succes)
+*/
 int main(void)
 {
 	int letter;
-	for(letter = 'a';letter <= 'z';letter++)
+	
+	for (letter = 'a';letter <= 'z';letter++)
 	{
 		letter = tolower(letter);
 		putchar(letter);
-		if(letter == 'z')
+		if (letter == 'z')
 		{
 			letter = 'A';
-			for(; letter <= 'Z';letter++)
+			for (; letter <= 'Z';letter++)
 			{
 				putchar(letter);
 			}
 			break;
 		}
-	}putchar('\n');
+	}
+	putchar('\n');
+	
 	return (0);
 }
