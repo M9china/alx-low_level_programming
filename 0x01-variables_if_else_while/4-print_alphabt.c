ca5662f910ac5a32eb08421889f5fb7418cb9913
M9china <95371889+M9china@users.noreply.github.com>
2023-02-21 03:14:30 +0200

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 #include <ctype.h>
 /**
- *main - Entry point
- *Return: Always 0 (success)
- */
+*main - Entry point
+*Return: Always 0 (success)
+*/
 int main(void)
 {
 	int alpha_bt;
-	for(alpha_bt = 'a';alpha_bt <= 'z';alpha_bt++)
+	
+	for (alpha_bt = 'a';alpha_bt <= 'z';alpha_bt++)
 	{
-		if(alpha_bt == 'e' || alpha_bt == 'q')
+		if (alpha_bt == 'e' || alpha_bt == 'q')
 		{
 			continue;
 		}
 		putchar(alpha_bt);
 	}
 	putchar('\n');
+	
 	return (0);
 }
