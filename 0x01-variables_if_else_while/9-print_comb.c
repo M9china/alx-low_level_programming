11acf47636f8e89994b776d08daab24af33cf0ac
M9china <nqobilencaza2@gmail.com>
2023-02-20 16:57:34 -0800

    Hot fix

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,15 +4,15 @@
  *main - Entry point
  *Return: Always 0 (success)
 */
-int main(void)
+int main (void)
 {
 	int numerals;
 
-	for(numerals = '0'; numerals <= '9'; numerals++)
+	for (numerals = '0'; numerals <= '9'; numerals++)
 	{
 		putchar(numerals);
 
-		if(numerals == '9')
+		if (numerals == '9')
 		{
 			continue;
 		}
