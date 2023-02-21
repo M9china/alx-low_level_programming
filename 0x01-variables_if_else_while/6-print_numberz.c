a73be032bc5dfa5bfad1459e49c1159e425a4ca4
M9china <95371889+M9china@users.noreply.github.com>
2023-02-21 03:15:59 +0200

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <ctype.h>
 /**
- *main - Entry point
- *Return: Always 0 (success)
- */
+*main - Entry point
+*Return: Always 0 (success)
+*/
 int main(void)
 {
 	int numerals;
-	for(numerals = '0';numerals <= '9';numerals++)
+	
+	for (numerals = '0';numerals <= '9';numerals++)
 	{
 		putchar(numerals);
 	}
+	
 	putchar('\n');
+	
 	return (0);
 }
 
