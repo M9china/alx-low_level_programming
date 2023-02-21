9dd8606126aa58bb10acb8258dadae71835049f0
M9china <95371889+M9china@users.noreply.github.com>
2023-02-21 03:12:05 +0200

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 /**
- *main - Entry point
- *Retrun: Always 0 (success)
- */
+*main - Entry point
+*Retrun: Always 0 (success)
+*/
 int main(void)
 {
-	int x;
+	int x ;
+	
 	for(x = 'a';x <= 'z';x++)
 	{
 		putchar(x);
 	}
+	
 	putchar('\n');
+	
 	return (0);
 }
