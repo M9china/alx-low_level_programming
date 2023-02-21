384d53ed6ca5c57a618eaef7be2713e80a49c10f
M9china <95371889+M9china@users.noreply.github.com>
2023-02-21 03:17:38 +0200

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
 #include <ctype.h>
 /**
- *main - Entry point
- *Return: Always 0 (success)
- */
+*main - Entry point
+*Return: Always 0 (success)
+*/
 int main(void)
 {
 	int numerals;
-	for(numerals = '0';numerals <= '9'; numerals++)
+	
+	for (numerals = '0';numerals <= '9'; numerals++)
 	{
 		putchar(numerals);
-		if(numerals == '9')
+		
+		if (numerals == '9')
 		{
 			numerals = 'a';
-			for(; numerals <= 'f';numerals++)
+			for (; numerals <= 'f';numerals++)
 			{
 				putchar(numerals);
 			}
 			break;
 		}
 	}
+	
 	putchar('\n');
+	
 	return (0);
 }
