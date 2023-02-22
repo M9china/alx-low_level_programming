f2df8c7705e1c7ac78de10d752f2bb677ccff4ad
M9china <nqobilencaza2@gmail.com>
2023-02-22 11:50:32 -0800

    Hot fix

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include<unistd.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: prints alphabet in lower cases, followed by new line
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	for(int n = 'a'; n <= 'z'; n++)
+	{
+		_putchar("n");
+
+	}
+	_puthcar("\n");
+
+	return (0);
+}
+
