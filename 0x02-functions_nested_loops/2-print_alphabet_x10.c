79892d4e5ef5bbfcd8c715fa634ef668c08445bc
M9china <nqobilencaza2@gmail.com>
2023-02-22 13:11:51 -0800

    print alphabetx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ * Return:(void)
+ */
+void print_alphabet_x10(void)
+{
+	int n, i;
+	for (n = 1; n < 11; n++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			putchar(i);
+		}
+	
+		putchar('\n');
+		
+	}
+
+	return (0);
+}
+
