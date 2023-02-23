1793850e317aaa176988ced05eb12d1078f8ea2a
M9china <nqobilencaza2@gmail.com>
2023-02-23 14:39:02 -0800

    Hot fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-/**
-*main - Entry point
-*Description: prints with proper grammar, but the outcome is a piece of art,
-*Return: Always 0 (success)
-*/
-int main(void)
-{ 
-        printf("with proper grammar, but the outcome is a piece of art,\n");
 
-        return (0);
+/**
+ * main - Print a string
+ * Description: use printf
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
