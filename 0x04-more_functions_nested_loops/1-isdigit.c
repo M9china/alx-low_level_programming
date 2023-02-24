aadcf13317ca59c376ea74d1e40947fb2eff4ef7
M9china <nqobilencaza2@gmail.com>
2023-02-24 03:49:43 -0800

    hot fix

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,19 +2,15 @@
 
 /**
  * _isdigit - Entry point
- * Description: checks for digits from 0 to 9
- * &c: is the argument
- * Return: 1 if true and 0 if false
+ * Description: Checks for a digit between 0 & 9
+ * @c: is an argument
+ * Return: 1 if true, 0 if false
  */
 
 int _isdigit(int c)
 {
 	if (c > 47 && c < 58)
-	{
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }
