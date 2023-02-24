afa562e000aaa0b36e5ad92facc8ecd1c32c5b73
M9china <nqobilencaza2@gmail.com>
2023-02-24 04:08:41 -0800

    Hot fix

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -5,12 +5,12 @@
  * Description: multiply two intigers
  * &a: is the first intiger
  * &b: is the second intiger
- * Return: int
+ * Return: prod
  */
 
 int mul(int a, int b)
 {
-	int prod = a*b;
+	int prod = a * b;
 
 	return(prod);
 }
