c1034a8b56b6bd4df03630b6c653db250098f8c1
M9china <nqobilencaza2@gmail.com>
2023-02-24 04:12:14 -0800

    Hot fix

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,16 +1,13 @@
 #include "main.h"
 
 /**
- * mul - Entry point
- * Description: multiply two intigers
- * &a: is the first intiger
- * &b: is the second intiger
- * Return: prod
+ * mul - calculates the product of a and b
+ * @a: variable
+ * @b: variable
+ * Return: a * b
  */
 
 int mul(int a, int b)
 {
-	int prod = a * b;
-
-	return(prod);
+return (a * b);
 }
