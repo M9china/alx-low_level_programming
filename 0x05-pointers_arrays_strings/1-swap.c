d7797db3f6b5300191e0185ad1a3536d89a36a56
M9china <nqobilencaza2@gmail.com>
2023-02-27 15:52:55 -0800

    swap values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap - swaps the values of two intigers
+ * @a: first variable
+ * @b: second variable
+ * Return: void
+ */
+
+void swap(int *a, int *b)
+{
+	int i;
+
+	i = &a;
+	&a = &b;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
 void reset_to_98(int *n);
 void _putchar( char c);
+void swap(int *a, int *b);
 
