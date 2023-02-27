cce5e97f350a9083115b729e155c6ed2678dffc9
M9china <nqobilencaza2@gmail.com>
2023-02-27 15:45:40 -0800

    removed white spaces

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,19 +1,14 @@
 #include "main.h"
 
 /**
- * reset_to_98 - takes a pointer to an int
- *  and updates the value it points to to 98
- *  @n: is a variables
- *  Return: void
+ * reset_to_98 - updates values that an int points to
+ * @n: is a varaiable
+ * Return: void
  */
 
 void reset_to_98(int *n)
 {
 	int num = 98;
-	
+
 	*n = num;
 }
-	
-
-	
-
