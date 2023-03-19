a1699947060b37bc9afd8a157c12a4398c5eae39
M9china <nqobilencaza2@gmail.com>
2023-03-19 04:17:32 -0700

    answers to any calls

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+/**
+ * main - prints its name followed by a new line
+ * @argc - counts arguments
+ * @argv - array of pointers to string
+ * Return: 0
+ * */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
