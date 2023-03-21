4bb8eef88762e89b0ae878b7566cfd68c89c3696
M9china <nqobilencaza2@gmail.com>
2023-03-21 06:10:03 -0700

    all tasks

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it gets
+ * @argc: argument count
+ * @argv: arguments
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int i;
+
+	for (i = 0; i < arg; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio>
+#include <stdlib.h>
+
+/**
+ * main - gives product of two numbers
+ * @argc: argument count
+ * @argv: arguments
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int a, b;
+
+	if(argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	printf("%d\n", a * b);
+
+	return (0);
+}
