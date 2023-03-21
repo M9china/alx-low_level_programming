8f18d539356f50662121e2692d72a08e24e91b5d
M9china <nqobilencaza2@gmail.com>
2023-03-21 05:15:48 -0700

    used colon instead

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  * main - prints number of arguments passed to it
- * @argc - argument count
- * @argv -  arguments
+ * @argc: argument count
+ * @argv:  arguments
  * Return: 0
  * */
 
