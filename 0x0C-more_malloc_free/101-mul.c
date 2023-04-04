6cbf63eaa08ec130a30887226b6d9604b0e2419a
M9china <nqobilencaza2@gmail.com>
2023-04-04 21:59:37 +0200

    multiply two numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -43,11 +43,8 @@ char *big_multiply(char *s1, char *s2)
 	int l1, l2, a, b, c, x;
 
 	l1 = _strlen(s1);
-
 	l2 = _strlen(s2);
-
 	r = malloc(a = x = l1 + l2);
-
 	if (!r)
 		printf("Error\n"), exit(98);
 	while (a--)
@@ -61,9 +58,7 @@ char *big_multiply(char *s1, char *s2)
 			printf("Error\n"), exit(98);
 		}
 		a = s1[l1] - '0';
-
 		c = 0;
-
 		for (l2 = _strlen(s2) - 1; l2 >= 0; l2--)
 		{
 			if (!_isdigit(s2[l2]))
@@ -72,11 +67,8 @@ char *big_multiply(char *s1, char *s2)
 				printf("Error\n"), exit(98);
 			}
 			b = s2[l2] - '0';
-
 			c += r[l1 + l2 + 1] + (a * b);
-
 			r[l1 + l2 + 1] = c % 10;
-
 			c /= 10;
 		}
 		if (c)
@@ -102,11 +94,8 @@ int main(int argc, char **argv)
 		printf("Error\n"), exit(98);
 
 	x = _strlen(argv[1]) + _strlen(argv[2]);
-
 	r = big_multiply(argv[1], argv[2]);
-
 	c = 0;
-
 	a = 0;
 
 	while (c < x)
