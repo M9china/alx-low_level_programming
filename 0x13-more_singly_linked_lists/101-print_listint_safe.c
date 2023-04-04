622d969d92e9c9cf653be43310a4bcdfd6e02a07
M9china <nqobilencaza2@gmail.com>
2023-04-05 01:32:20 +0200

    print a linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -4,11 +4,12 @@
 
 /**
  * _r - reallocates memory for an array of pointers
+ * to the nodes in a linked list
  * @list: the old list to append
- * @size: size of the new list 
+ * @size: size of the new list
  * @new: new node to add to the list
  *
- * Return: pointer to new list
+ * Return: pointer to the new list
  */
 const listint_t **_r(const listint_t **list, size_t size, const listint_t *new)
 {
@@ -29,14 +30,15 @@ const listint_t **_r(const listint_t **list, size_t size, const listint_t *new)
 }
 
 /**
- * print_listint_safe - prints a linked list.
+ * print_listint_safe - prints linked list.
  * @head: pointer to the start of the list
  *
- * Return: node count
+ * Return: node count in list
  */
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t i, num = 0;
+
 	const listint_t **list = NULL;
 
 	while (head != NULL)
