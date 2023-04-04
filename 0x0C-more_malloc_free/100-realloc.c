2129e1a96276267d175987685aa056e8efd4843c
M9china <nqobilencaza2@gmail.com>
2023-04-04 21:43:45 +0200

    relocate memory block using malloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -3,7 +3,7 @@
 #include<stdlib.h>
 
 /**
- * *_realloc - reallocates memory block
+ * *_realloc - function reallocates memory block
  *
  *@ptr: void pointer
  *@old_size: unsigned int
@@ -20,46 +20,35 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int i;
 
 	if (new_size == old_size)
-
 		return (ptr);
-
 	if (ptr == NULL)
 	{
 		ptr = malloc(new_size);
-
-		if (ptr == NULL
-				)
+		if (ptr == NULL)
 			return (NULL);
-
 		return (ptr);
 	}
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
-
 		return (NULL);
 	}
 	newp = malloc(new_size);
-
 	if (newp == NULL)
 	{
 		free(ptr);
-
 		return (NULL);
 	}
 	if (new_size < old_size)
 	{
 		for (i = 0; i < old_size; i++)
-
 			newp[i] = copyp[i];
 	}
 	if (new_size > old_size)
 	{
 		for (i = 0; i < old_size; i++)
-
 			newp[i] = copyp[i];
 	}
 	free(ptr);
-
 	return (newp);
 }
