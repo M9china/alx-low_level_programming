0e9cf3995ee36ba3e6ce8fbceccb39ce1e5d3df8
M9china <nqobilencaza2@gmail.com>
2023-06-17 01:59:41 -0700

    added an else statement

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -22,10 +22,12 @@ int i, j;
 			if (argv[i][j] > 57 || argv[i][j] < 48)
 			{  printf("Error\n");
 			exit(98); }
+
+			else {
+				mul = atol(argv[1]) *  atol(argv[2]);
 		}
 
 	}
-	mul = atol(argv[1]) *  atol(argv[2]);
 	printf("%lu\n", mul);
 return (0);
 }
