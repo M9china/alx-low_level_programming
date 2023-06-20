d0fd1d197f84df92798873bebcaed7b6c4ecc295
M9china <nqobilencaza2@gmail.com>
2023-06-20 04:09:46 -0700

    hot fix

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - prints the name of a file
+ * main - preints the name of the file
  *
- * Return: 0 (Success)
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	printf("%s\n", __FILE__);
-	
 	return (0);
 }
