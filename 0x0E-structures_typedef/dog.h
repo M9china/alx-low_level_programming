55b14e8a19afeef39b64da4fa6f17e3f110e32c9
M9china <nqobilencaza2@gmail.com>
2023-06-20 08:49:14 -0700

    defined a new type structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog objects
+ * @name: name of the dog
+ * age: age of the dog
+ * @owner: caretaker of the dog
+ *
+ * Description: The dog objects 
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+
+#endif
