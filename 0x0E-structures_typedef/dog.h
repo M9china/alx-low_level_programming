a0fe0ef3758b39f146e63fe4f8be5f039dcda79b
M9china <nqobilencaza2@gmail.com>
2023-06-20 09:30:54 -0700

    chore:betty coding style

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,14 +7,14 @@
  * @age: The age of the dog.
  * @owner: The owner of the dog.
  *
- * Description: The attributes of a dog. 
+ * Description: The attributes of a dog.
  */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-
 };
+
 
 #endif
