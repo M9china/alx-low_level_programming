14865e7d69f5a466fc36f65fb947f14e22058552
M9china <nqobilencaza2@gmail.com>
2023-06-21 08:01:32 -0700

    created a function that searches for intenger

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,7 +4,6 @@
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
-
-#include <stdio.h>
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
