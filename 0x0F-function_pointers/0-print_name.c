23e31f800d0b57e2ad3e613665604a04d988022f
M9china <nqobilencaza2@gmail.com>
2023-06-21 07:10:49 -0700

    removed white spaces

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,16 +1,14 @@
 #include "function_pointers.h"
-#include <stdlib.h>
-
+#include<stdlib.h>
 /**
- * print_name - Prints a name
- * @name: The name that is printed.
- * @f: The function pointer.
- * 
+ * print_name - prints a name
+ * @name: person's name
+ * @f: pointer to a function
  * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (f != NULL)
-	f(name);
+if (f != NULL)
+f(name);
 
 }
