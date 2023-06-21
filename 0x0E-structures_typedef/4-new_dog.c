3779342c363c341f5a9606d86af261dfd3f641f3
M9china <nqobilencaza2@gmail.com>
2023-06-20 22:56:34 -0700

    added missing functions

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -50,3 +50,40 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	return (cutie_dog);
 }
+
+/**
+ * *_strcpy -  copies the string pointed to by src
+ * @dest: char type string
+ * @src: char type string
+ * Description: Copy the string pointed to by pointer `src` to
+ * the buffer pointed to by `dest`
+ * Return: Pointer to `dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = -1;
+
+	do {
+		i++;
+		dest[i] = src[i];
+	} while (src[i] != '\0');
+
+	return (dest);
+}
+
+/**
+ * _strlen - checks the length of a string
+ * @s: string to check
+ *
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length])
+		length++;
+	return (length);
+}
