c6fb4428ed9057548078ff79ac54f97c4fac56b8
M9china <nqobilencaza2@gmail.com>
2023-06-29 12:46:48 -0700

    resloved conflicts

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,9 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-<<<<<<< HEAD
-size_t print_list(const list_t *h);
-=======
 /**
  * struct list_s - singly linked list
  * @str: string 
@@ -23,6 +20,5 @@ typedef struct list_s
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
->>>>>>> 18d301caf2d36bb56a6d4e3e3395a84560de7062
 
 #endif
