64e7eac034d2bdb4ae8cd70e1b73dde9bd06c0c0
M9china <nqobilencaza2@gmail.com>
2023-07-05 16:44:41 -0700

    fix: defined listint_t

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,8 +1,15 @@
-#include <stddef.h>
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stddef.h>
+
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
 size_t print_listint(const listint_t *h);
 
-#endif
-	
+#endif /* LISTS_H */
+
