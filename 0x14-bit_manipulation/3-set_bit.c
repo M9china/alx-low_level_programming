613801891da4f80a671e5f89edda803a87cf6236
M9china <nqobilencaza2@gmail.com>
2023-07-07 19:06:38 -0700

    doc:betty coding style

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/** set_bit - sets bit at index to 1
+ * @n: pointer to index
+ * @index: position of a bit
+ *
+ * Return: 1 on success, or -1 if fails
+ */
+
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int j;
