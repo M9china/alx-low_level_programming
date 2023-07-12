16ae8297a71056f7319dcc20bb958851ea232288
M9china <nqobilencaza2@gmail.com>
2023-07-11 17:25:05 -0700

    fix: mixed declarations and code

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,27 +1,35 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
+
 /**
- * append_text_to_file - Appends text to the end of a file.
+ * append_text_to_file - appends text at the end of a file.
+ * @filename:  name of the file.
+ * @text_content: NULL terminated string to add at end of file
  *
- * @filename: The name of the file to append to.
- * @text_content: The text to append to the file.
- *
- * Return: 1 on success, -1 on failure.
+ * Return: 1 on success
+ *         -1 on failure.
  */
 
-int append_text_to_file(const char *filename, char *text_content) {
-    if (filename == NULL)
-        return -1;
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int op, wr, len = 0;
 
-    FILE *file = fopen(filename, "a");
-    if (file == NULL)
-        return -1;
+	if (filename == NULL)
+		return (-1);
 
-    if (text_content != NULL) {
-        fputs(text_content, file);
-    }
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 
-    fclose(file);
-    return 1;
+	op = open(filename, O_WRONLY | O_APPEND);
+	wr = write(op, text_content, len);
+
+	if (op == -1 || wr == -1)
+		return (-1);
+
+	close(op);
+
+	return (1);
 }
 
