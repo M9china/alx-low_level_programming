3ca34016446c607b02e9d3df4a7a5fb416024280
M9china <nqobilencaza2@gmail.com>
2023-08-03 16:28:59 -0700

    hot fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,24 +10,25 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-        int a;
-        unsigned int result = 0;
+	int a;
+	unsigned int result = 0;
 
-        if (!b)
-                return (0);
+	if (!b)
+		return (0);
 
-        for (a = 0; b[a] != '\0'; a++)
-        {
-                if (b[a] != '0' && b[a] != '1')
-                        return (0);
-        }
+	for (a = 0; b[a] != '\0'; a++)
+	{
+		if (b[a] != '0' && b[a] != '1')
+			return (0);
+	}
 
-        for (a = 0; b[a] != '\0'; a++)
-        {
-                result <<= 1;
-                if (b[a] == '1')
-                        result += 1;
-        }
+	for (a = 0; b[a] != '\0'; a++)
+	{
+		result <<= 1;
+		if (b[a] == '1')
+			result += 1;
+	}
 
-        return (result);
+	return (result);
 }
+
