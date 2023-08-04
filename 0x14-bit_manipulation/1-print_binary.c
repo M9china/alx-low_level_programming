ee967b0c2ebc38848477bd8f17f7b553e00eeb67
M9china <nqobilencaza2@gmail.com>
2023-08-04 05:06:54 -0700

    removed unused code

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -15,21 +15,3 @@ void print_binary(unsigned long int n)
     /* printing the least significant bit using AND(n & 1) */
 }
 
-#include "main.h"
-
-/**
- * get_endianness - Checks the endianness.
- * Return: If big-endian - 0.
- * If little-endian - 1.
- */
-int get_endianness(void)
-{
-    int num = 1;
-    char *endian = (char *)&num;
-
-    if (*endian == 1)
-        return (1);
-
-    return (0);
-}
-
