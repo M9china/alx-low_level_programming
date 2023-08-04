9663dd0daa5e0637f2541b7c32e80b1de25e65dd
M9china <nqobilencaza2@gmail.com>
2023-08-04 04:34:57 -0700

    fix: indentation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,19 +2,19 @@
 #include "main.h"
 
 /**
- * print_binary -  prints the binary representation of a number.
+ * print_binary - prints the binary representation of a number.
  * @n: number to be printed in binary.
  */
-
 void print_binary(unsigned long int n)
 {
     if (n > 1)
-	print_binary(n >> 1);
-        /*recursively calling print_binary with n right shifted  by 1 bit*/
+        print_binary(n >> 1);
+    /* recursively calling print_binary with n right shifted by 1 bit */
 
     putchar((n & 1) + '0');
-        /*printing the least significant bit using AND(n & 1)*/
+    /* printing the least significant bit using AND(n & 1) */
 }
+
 #include "main.h"
 
 /**
@@ -22,11 +22,9 @@ void print_binary(unsigned long int n)
  * Return: If big-endian - 0.
  * If little-endian - 1.
  */
-
 int get_endianness(void)
 {
     int num = 1;
-
     char *endian = (char *)&num;
 
     if (*endian == 1)
@@ -34,3 +32,4 @@ int get_endianness(void)
 
     return (0);
 }
+
