c9449ff1874f9b32dd0ae4d61adb2ef87ae7b670
M9china <nqobilencaza2@gmail.com>
2023-08-24 12:48:11 -0700

    script that creates dynamic lib

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-# Get all the .c files in the current directory
-c_files=$(ls *.c)
+gcc -Wall -fPIC -c *.c
 
-# Compile each .c file into an object file
-for file in $c_files; do
-    gcc -c $file -o ${file%.c}.o
-done
+gcc -shared -o liball.so *.o
 
-# Create the static library
-ar rcs liball.a *.o
+rm *.o
 
-	
